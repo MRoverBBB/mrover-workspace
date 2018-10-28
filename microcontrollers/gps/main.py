72a372b6091ad9622a7081a01f5e761fe50bec31
raytitan <raytitan@umich.edu>
2018-10-28 13:00:54 -0400

    Removed LCM and added project,ini

--- microcontrollers/gps/main.py
+++ microcontrollers/gps/main.py
@@ -1,9 +1,9 @@
 import serial
-from rover_common import aiolcm
-from rover_msgs import GPSOdometry
+#from rover_common import aiolcm
+#from rover_msgs import GPSOdometry
 
 def main():
-    lcm = aiolcm.AsyncLCM()
+ #   lcm = aiolcm.AsyncLCM()
 
     serialPort = serial.Serial("Some name")
     serialPort.baudrate = 19200
@@ -15,10 +15,15 @@ def main():
         if (oneByte == '$'):
             fiveBytes = serialPort.read(5);
             if (fiveBytes == 'GPRMC'):
-                gpsOdometry = GPSOdometry()
-                moreBytes = serialPort.read(51)
-                gpsOdometry.lattitude = float(moreBytes[12:22])
-                gpsOdometry.longitude = float(moreBytes[25:36])
-                gpsOdometry.speed = float(moreBytes[39:45])
-                gpsOdometry.bearing = float(moreBytes[46:51])
-                lcm.publish('/some channel', gpsOdometry.encode())
+#                gpsOdometry = GPSOdometry()
+#                moreBytes = serialPort.read(51)
+#                gpsOdometry.lattitude = float(moreBytes[12:22])
+#                gpsOdometry.longitude = float(moreBytes[25:36])
+#                gpsOdometry.speed = float(moreBytes[39:45])
+#                gpsOdometry.bearing = float(moreBytes[46:51])
+#                lcm.publish('/some channel', gpsOdometry.encode())
+                print("Lattitude" + float(moreBytes[12:22]))
+                print("Longtitude" + float(moreBytes[25:36]))
+                print("Speed" + float(moreBytes[39:45]))
+                print("Bearing" + float(moreBytes[46:51]))
+                
