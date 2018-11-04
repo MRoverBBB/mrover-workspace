29f29fd2a57ca63bcf8ecc0165389b77c6f048c6
raytitan <raytitan@umich.edu>
2018-11-04 15:05:54 -0500

    MAG ACCEL GYRO FINISHED

---
+++ microcontrollers/accel_mag_gyro/main.py
@@ -0,0 +1,28 @@
+import serial
+import time
+from rover_common import aiolcm
+from rover_msgs import AMG
+
+lcm_ = aiolcm.AsyncLCM()
+
+def main():
+    serialPort = serial.Serial("/dev/ttyS0")
+    serialPort.baudrate = 115200
+
+    while (True):
+        c = serialPort.read()
+        if(c == '$'):
+			amgMsg = AMG()
+            amgMsg.accel_x = float(serialPort.read_until(',')[:-1])
+            amgMsg.accel_y = float(serialPort.read_until(',')[:-1])
+            amgMsg.accel_z = float(serialPort.read_until(',')[:-1])
+            amgMsg.mag_x = float(serialPort.read_until(',')[:-1])
+            amgMsg.mag_y = float(serialPort.read_until(',')[:-1])
+            amgMsg.mag_z = float(serialPort.read_until(',')[:-1])
+            amgMsg.gyro_x = float(serialPort.read_until(',')[:-1])
+            amgMsg.gyro_y = float(serialPort.read_until(',')[:-1])
+            amgMsg.gyro_z = float(serialPort.read_until(',')[:-1])
+            _lcm.publish('/amg', amgMsg.encode())
+            
+if __name__ == "__main__":
+    main()
--- microcontrollers/gps/main.py
+++ microcontrollers/gps/main.py
@@ -1,28 +1,46 @@
+#!/usr/bin/python
 import serial
 #from rover_common import aiolcm
 #from rover_msgs import GPSOdometry
 
 def main():
  #   lcm = aiolcm.AsyncLCM()
-
-    serialPort = serial.Serial("/dev/ttyAMA0")
-    serialPort.baudrate = 19200
-    serialPort.open()
-
-    while (true):
-        oneByte = serialPort.read();
+    print("Hello World!")
+    serialPort = serial.Serial("/dev/ttyUSB0")
+    serialPort.baudrate = 4800
+    serialPort.bytesize = serial.EIGHTBITS
+    serialPort.parity = serial.PARITY_NONE
+    serialPort.stopbits = serial.STOPBITS_ONE
+    
+    #serialPort.open()
+    #print(serialPort.write("$PGRMI,4229.34651,N,8371.11319.W,041118,180342.9,R\r\n".encode()))
+    #print(serialPort.write(bytearray.fromhex('100A022600CE1003')))
+    #print(serialPort.write('$PGRMC1,,1,,,,,,,,,,,,\r\n'.encode()))
+#    print(serialPort.write(b'$PGRMO,,2\r\n'))
+#    print(serialPort.write(b'$PGRMC1,,1,,,,,,,,,,,,,\r\n'))
+    while (True):
+        #print("Entering Loop")
+        oneByte = serialPort.read()
+        print(oneByte),
         if (oneByte == '$'):
+            print("Test1")
             fiveBytes = serialPort.read(5);
             if (fiveBytes == 'GPRMC'):
 #               gpsOdometry = GPSOdometry()
-                moreBytes = serialPort.read(51)
 #               gpsOdometry.lattitude = float(moreBytes[12:22])
 #               gpsOdometry.longitude = float(moreBytes[25:36])
 #               gpsOdometry.speed = float(moreBytes[39:45])
 #               gpsOdometry.bearing = float(moreBytes[46:51])
 #               lcm.publish('/some channel', gpsOdometry.encode())
-                print("Lattitude " + str(float(moreBytes[12:22])))
-                print("Longtitude " + str(float(moreBytes[25:36])))
-                print("Speed " + str(float(moreBytes[39:45])))
-                print("Bearing " + str(float(moreBytes[46:51])))
-                
+				serialPort.read_until(',')[:-1]
+				serialPort.read_until(',')[:-1]
+				serialPort.read_until(',')[:-1]
+				lattitude = serialPort.read_until(',')[:-1]
+				serialPort.read_until(',')[:-1]
+				longitude = serialPort.read_until(',')[:-1]
+				print("Lattitude " + lattitude)
+				print("Longtitude " + longitude)
+
+if __name__ == "__main__":
+    main()
+
