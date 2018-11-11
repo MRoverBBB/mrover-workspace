e8c5b21f73cbbcc249a89d3ea925901a83d2aa89
raytitan <raytitan@umich.edu>
2018-11-11 14:07:06 -0500

    Rewrote for python3

--- microcontrollers/accel_mag_gyro/main.py
+++ microcontrollers/accel_mag_gyro/main.py
@@ -6,23 +6,23 @@ from rover_msgs import AMG
 lcm_ = aiolcm.AsyncLCM()
 
 def main():
-    serialPort = serial.Serial("/dev/ttyS0")
-    serialPort.baudrate = 115200
+	serialPort = serial.Serial("/dev/ttyS0")
+	serialPort.baudrate = 115200
 
-    while (True):
-        c = serialPort.read()
-        if(c == '$'):
+	while (True):
+		c = serialPort.read()
+		if(c == '$'):
 			amgMsg = AMG()
-            amgMsg.accel_x = float(serialPort.read_until(',')[:-1])
-            amgMsg.accel_y = float(serialPort.read_until(',')[:-1])
-            amgMsg.accel_z = float(serialPort.read_until(',')[:-1])
-            amgMsg.mag_x = float(serialPort.read_until(',')[:-1])
-            amgMsg.mag_y = float(serialPort.read_until(',')[:-1])
-            amgMsg.mag_z = float(serialPort.read_until(',')[:-1])
-            amgMsg.gyro_x = float(serialPort.read_until(',')[:-1])
-            amgMsg.gyro_y = float(serialPort.read_until(',')[:-1])
-            amgMsg.gyro_z = float(serialPort.read_until(',')[:-1])
-            _lcm.publish('/amg', amgMsg.encode())
+			amgMsg.accel_x = float(serialPort.read_until(',')[:-1])
+			amgMsg.accel_y = float(serialPort.read_until(',')[:-1])
+			amgMsg.accel_z = float(serialPort.read_until(',')[:-1])
+			amgMsg.mag_x = float(serialPort.read_until(',')[:-1])
+			amgMsg.mag_y = float(serialPort.read_until(',')[:-1])
+			amgMsg.mag_z = float(serialPort.read_until(',')[:-1])
+			amgMsg.gyro_x = float(serialPort.read_until(',')[:-1])
+			amgMsg.gyro_y = float(serialPort.read_until(',')[:-1])
+			amgMsg.gyro_z = float(serialPort.read_until(',')[:-1])
+			_lcm.publish('/amg', amgMsg.encode())
             
 if __name__ == "__main__":
     main()
