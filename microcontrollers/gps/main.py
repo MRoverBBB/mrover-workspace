8d5aace37dfd7ccfa3bd34da3785f71658e3913b
raytitan <raytitan@umich.edu>
2018-11-11 14:03:38 -0500

    Changed lcm message

--- microcontrollers/gps/main.py
+++ microcontrollers/gps/main.py
@@ -1,45 +1,39 @@
 #!/usr/bin/python
 import serial
 #from rover_common import aiolcm
-#from rover_msgs import GPSOdometry
+#from rover_msgs import Odometry
 
 def main():
  #   lcm = aiolcm.AsyncLCM()
-    print("Hello World!")
-    serialPort = serial.Serial("/dev/ttyUSB0")
-    serialPort.baudrate = 4800
-    serialPort.bytesize = serial.EIGHTBITS
-    serialPort.parity = serial.PARITY_NONE
-    serialPort.stopbits = serial.STOPBITS_ONE
+	print("Hello World!")
+	serialPort = serial.Serial("/dev/ttyUSB1")
+	serialPort.baudrate = 4800
+	serialPort.bytesize = serial.EIGHTBITS
+	serialPort.parity = serial.PARITY_NONE
+	serialPort.stopbits = serial.STOPBITS_ONE
     
-    #serialPort.open()
-    #print(serialPort.write("$PGRMI,4229.34651,N,8371.11319.W,041118,180342.9,R\r\n".encode()))
-    #print(serialPort.write(bytearray.fromhex('100A022600CE1003')))
-    #print(serialPort.write('$PGRMC1,,1,,,,,,,,,,,,\r\n'.encode()))
-#    print(serialPort.write(b'$PGRMO,,2\r\n'))
-#    print(serialPort.write(b'$PGRMC1,,1,,,,,,,,,,,,,\r\n'))
-    while (True):
-        #print("Entering Loop")
-        oneByte = serialPort.read()
-        print(oneByte),
-        if (oneByte == '$'):
-            print("Test1")
-            fiveBytes = serialPort.read(5);
-            if (fiveBytes == 'GPRMC'):
-#               gpsOdometry = GPSOdometry()
-#               gpsOdometry.lattitude = float(moreBytes[12:22])
-#               gpsOdometry.longitude = float(moreBytes[25:36])
-#               gpsOdometry.speed = float(moreBytes[39:45])
-#               gpsOdometry.bearing = float(moreBytes[46:51])
-#               lcm.publish('/some channel', gpsOdometry.encode())
+	while (True):
+		oneByte = serialPort.read()
+		if (oneByte == '$'):
+			fiveBytes = serialPort.read(5);
+			if (fiveBytes == 'GPRMC'):
+#               Odometry = Odometry()
 				serialPort.read_until(',')[:-1]
 				serialPort.read_until(',')[:-1]
 				serialPort.read_until(',')[:-1]
-				lattitude = serialPort.read_until(',')[:-1]
+				latitude = float(serialPort.read_until(',')[:-1])
 				serialPort.read_until(',')[:-1]
-				longitude = serialPort.read_until(',')[:-1]
-				print("Lattitude " + lattitude)
-				print("Longtitude " + longitude)
+				longitude = float(serialPort.read_until(',')[:-1])
+				serialPort.read_until(',')[:-1]
+				speed = float(serialPort.read_until(',')[:-1])
+				bearing = float(serialPort.read_until(',')[:-1])
+#				Odometry.latitude_deg = int(latitude/100)
+#				Odometry.longitude_deg = int(longitude/100)
+#				Odometry.latitude_min = latitude - (Odometry.latitude_deg * 100)
+#				Odometry.longitude_min = longitude  - (Odometry.longitude_deg * 100)
+#				Odometry.bearing_deg = bearing
+#				Odometry.speed = speed
+#               lcm.publish('/some channel', Odometry.encode())
 
 if __name__ == "__main__":
     main()
